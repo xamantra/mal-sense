88624f3d15e93083bee6bf45362ecad102eb93bb
xamantra <xamantrx@gmail.com>
2020-09-10 21:59:33 +0800

    adjust logger

--- src/bootstrapper.js
+++ src/bootstrapper.js
@@ -3,7 +3,7 @@ class Bootstrapper {
   constructor(paths = [], name) {
     this._paths = paths
     this._name = name
-    this._version = '1.0.5'
+    this._version = '1.0.6'
   }
 
   start(callback) {
@@ -14,17 +14,16 @@ class Bootstrapper {
       console.log(`Checking url for path "${path}"`)
       if (url.includes(path)) {
         canStart = true
-        break
       }
 
-      if (i === (this._paths.length - 1) && !canStart) {
-        console.log(`Skipping bootstrap on page "${window.location.href}" for "${this._name}".`)
+      if (i === (this._paths.length - 1)) {
+        if (canStart && !callback) {
+          console.log(`[MAL Sense - v${this._version}] - Starting "${this._name}" ...`)
+          callback()
+        } else {
+          console.log(`Skipping bootstrap on page "${window.location.href}" for "${this._name}".`)
+        }
       }
-    }
-
-    if (canStart && !callback) {
-      console.log(`[MAL Sense - v${this._version}] - Starting "${this._name}" ...`)
-      callback()
     }
   }
 }
