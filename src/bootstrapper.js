7a95a7e046abc97e87481cb6d320ba7c0115d7b1
xamantra <xamantrx@gmail.com>
2020-09-10 22:02:53 +0800

    simplified bootstrapper

--- src/bootstrapper.js
+++ src/bootstrapper.js
@@ -3,26 +3,20 @@ class Bootstrapper {
   constructor(paths = [], name) {
     this._paths = paths
     this._name = name
-    this._version = '1.0.6'
+    this._version = '1.0.7'
   }
 
   start(callback) {
     const url = window.location.href.trim().toLowerCase()
-    let canStart = false
     for (let i = 0; i < this._paths.length; i++) {
       const path = this._paths[i].trim().toLowerCase()
       console.log(`Checking url for path "${path}"`)
       if (url.includes(path)) {
-        canStart = true
-      }
-
-      if (i === (this._paths.length - 1)) {
-        if (canStart && !callback) {
-          console.log(`[MAL Sense - v${this._version}] - Starting "${this._name}" ...`)
-          callback()
-        } else {
-          console.log(`Skipping bootstrap on page "${window.location.href}" for "${this._name}".`)
-        }
+        console.log(`[MAL Sense - v${this._version}] - Starting "${this._name}" ...`)
+        callback()
+        break
+      } else if (i === (this._paths.length - 1)) {
+        console.log(`Skipping bootstrap on page "${window.location.href}" for "${this._name}".`)
       }
     }
   }
